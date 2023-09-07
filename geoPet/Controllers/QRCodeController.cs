0de133f0a45ca06670126e8a1cf80315d69b9b43
Tamires Sousa <tamireshc@hotmail.com>
2023-09-07 11:29:24 -0300

    feat: add routes for QRCode with informatons for pets and owers

---
+++ geoPet/Controllers/QRCodeController.cs
@@ -0,0 +1,41 @@
+﻿using geoPet.Services;
+using geoPet.Utils;
+using Microsoft.AspNetCore.Mvc;
+
+namespace geoPet.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class QRCodeController : ControllerBase
+    {
+        private OwerService _owerService;
+        private PetService _petService;
+
+        public QRCodeController(OwerService service, PetService petService)
+        {
+            _owerService = service;
+            _petService = petService;
+        }
+
+
+        [HttpGet("Pet/{id}")]
+        public IActionResult getPetQrCode(int id)
+        {
+            _petService.findById(id);
+
+            QRCodeGenerator QRCodeGenerator = new QRCodeGenerator();
+            var image = QRCodeGenerator.GenerateQrCode("Pet", id.ToString());
+            return File(image, "image/jpeg");
+        }
+
+        [HttpGet("Ower/{id}")]
+        public IActionResult getOwerQrCode(int id)
+        {
+            _owerService.findById(id);
+
+            QRCodeGenerator QRCodeGenerator = new QRCodeGenerator();
+            var image = QRCodeGenerator.GenerateQrCode("Ower", id.ToString());
+            return File(image, "image/jpeg");
+        }
+    }
+}
---
+++ geoPet/Utils/QRCodeGenerator.cs
@@ -0,0 +1,30 @@
+﻿using QRCoder;
+
+namespace geoPet.Utils
+{
+    //https://balta.io/blog/aspnet-qrcode
+    public class QRCodeGenerator
+    {
+        public byte[] GenerateQrCode(string type, string id)
+        {
+            try
+            {
+                QRCoder.QRCodeGenerator qrGenerator = new QRCoder.QRCodeGenerator();
+                QRCodeData qrCodeData = qrGenerator.CreateQrCode("https://localhost:7225/" + type + "/" + id, QRCoder.QRCodeGenerator.ECCLevel.Q);
+
+                // Crie um objeto QRCode com base nos dados e personalize-o
+                var qrCode = new QRCoder.BitmapByteQRCode(qrCodeData);
+
+                var qrCodeImage = qrCode.GetGraphic(20); // 20 é o tamanho do módulo (pixel)
+
+                return qrCodeImage;
+            }
+            catch (Exception e)
+            {
+                // Lidar com erros 
+                throw new Exception("Erro gennereted QRCode");
+            }
+        }
+    }
+}
+
