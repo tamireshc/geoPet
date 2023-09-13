887a41bfb7c0b422f503fd72204b35d46490a20f
Tamires Sousa <tamireshc@hotmail.com>
2023-09-13 08:44:02 -0300

    feat: add testes for delete pet route and init testes for update pet

--- geoPet.Test/PetControllerTest.cs
+++ geoPet.Test/PetControllerTest.cs
@@ -198,44 +198,92 @@ namespace geoPet.Test
             stringResult.Should().Contain("Pet not found");
         }
 
-        //[Fact]
-        //public async Task UpdateOwer()
-        //{
-        //    using (var scope = _factory.Services.CreateScope())
-        //    {
-        //        var dbContext = scope.ServiceProvider.GetRequiredService<GeoPetContext>();
+        [Fact]
+        public async Task UpdatePet()
+        {
+            using (var scope = _factory.Services.CreateScope())
+            {
+                var dbContext = scope.ServiceProvider.GetRequiredService<GeoPetContext>();
 
-        //        dbContext.Database.EnsureDeleted();
-        //        dbContext.Database.EnsureCreated();
+                dbContext.Database.EnsureDeleted();
+                dbContext.Database.EnsureCreated();
 
-        //        //add a new ower
-        //        var jsonToAdd = "{\"name\":\"Yuri\",\"email\":\"yuri@gmail.com\",\"cep\":\"31525380\",\"password\":\"yuri\"}";
-        //        var stringContent = new StringContent(jsonToAdd, Encoding.UTF8, "application/json");
-        //        var result = await _client.PostAsync("/Ower", stringContent);
-        //        result.StatusCode.Should().Be((System.Net.HttpStatusCode)200);
+                //add  ower
+                var jsonToAdd = "{\"name\":\"Yuri\",\"email\":\"yuri@gmail.com\",\"cep\":\"31525380\",\"password\":\"yuri\"}";
+                var stringContent = new StringContent(jsonToAdd, Encoding.UTF8, "application/json");
+                var result = await _client.PostAsync("/Ower", stringContent);
+                result.StatusCode.Should().Be((System.Net.HttpStatusCode)200);
 
-        //        //att this new ower
-        //        var jsonToAdd2 = "{\"name\":\"Tamires\",\"email\":\"tamires@gmail.com\",\"cep\":\"31525380\",\"password\":\"tamires\"}";
-        //        var stringContent2 = new StringContent(jsonToAdd2, Encoding.UTF8, "application/json");
-        //        var result2 = await _client.PutAsync("/Ower/1", stringContent2);
+                //add  pet
+                var jsonToAdd2 = "{\"name\":\"JUJUBA\",\"age\":2,\"size\":\"MEDIUM\",\"breed\":\"VIRA-LATA\",\"owerId\":1}";
+                var stringContent2 = new StringContent(jsonToAdd2, Encoding.UTF8, "application/json");
+                var result2 = await _client.PostAsync("/Pet", stringContent2);
+                result2.StatusCode.Should().Be((System.Net.HttpStatusCode)200);
 
-        //        result2.StatusCode.Should().Be((System.Net.HttpStatusCode)200);
-        //        var stringResult = result2.Content.ReadAsStringAsync().Result;
-        //    }
+                //att this new pet
+                var jsonToAdd3 = "{\"name\":\"Damiao\",\"age\":2,\"size\":\"MEDIUM\",\"breed\":\"VIRA-LATA\",\"owerId\":1}";
+                var stringContent3 = new StringContent(jsonToAdd3, Encoding.UTF8, "application/json");
+                var result3 = await _client.PutAsync("/Pet/1", stringContent3);
 
-        //}
+                result2.StatusCode.Should().Be((System.Net.HttpStatusCode)200);
+ 
+                //Get pets by id 1
+                var result4 = await _client.GetAsync("/Pet/1");
+                result4.StatusCode.Should().Be((System.Net.HttpStatusCode)200);
+                var stringResult = result4.Content.ReadAsStringAsync().Result;
 
-        //[Fact]
-        //public async Task UpdateOwerWithNonExistentOwer()
-        //{
-        //    var jsonToAdd = "{\"name\":\"Yuri\",\"email\":\"yuri@gmail.com\",\"cep\":\"31525380\",\"password\":\"yuri\"}";
-        //    var stringContent = new StringContent(jsonToAdd, Encoding.UTF8, "application/json");
-        //    var result = await _client.PutAsync("/Ower/99", stringContent);
-        //    result.StatusCode.Should().Be((System.Net.HttpStatusCode)404);
+                stringResult.Should().Contain("Damiao");
+                stringResult.Should().Contain("VIRA-LATA");
+                stringResult.Should().Contain("1");
+            }
 
-        //    var stringResult = result.Content.ReadAsStringAsync().Result;
-        //    stringResult.Should().Contain("Ower not found");
-        //}
+        }
+
+        [Fact]
+        public async Task UpdatePetWithNonExistentPetId()
+        {
+
+            var jsonToAdd = "{\"name\":\"Damiao\",\"age\":2,\"size\":\"MEDIUM\",\"breed\":\"VIRA-LATA\",\"owerId\":1}";
+            var stringContent = new StringContent(jsonToAdd, Encoding.UTF8, "application/json");
+            var result = await _client.PutAsync("/Pet/99", stringContent);
+            result.StatusCode.Should().Be((System.Net.HttpStatusCode)404);
+
+            var stringResult = result.Content.ReadAsStringAsync().Result;
+            stringResult.Should().Contain("Pet not found");
+        }
+
+        [Fact]
+        public async Task UpdatePetWithNonExistentOwerId()
+        {
+            using (var scope = _factory.Services.CreateScope())
+            {
+                var dbContext = scope.ServiceProvider.GetRequiredService<GeoPetContext>();
+
+                dbContext.Database.EnsureDeleted();
+                dbContext.Database.EnsureCreated();
+
+                //add  ower
+                var jsonToAdd = "{\"name\":\"Yuri\",\"email\":\"yuri@gmail.com\",\"cep\":\"31525380\",\"password\":\"yuri\"}";
+                var stringContent = new StringContent(jsonToAdd, Encoding.UTF8, "application/json");
+                var result = await _client.PostAsync("/Ower", stringContent);
+                result.StatusCode.Should().Be((System.Net.HttpStatusCode)200);
+
+                //add  pet
+                var jsonToAdd2 = "{\"name\":\"JUJUBA\",\"age\":2,\"size\":\"MEDIUM\",\"breed\":\"VIRA-LATA\",\"owerId\":1}";
+                var stringContent2 = new StringContent(jsonToAdd2, Encoding.UTF8, "application/json");
+                var result2 = await _client.PostAsync("/Pet", stringContent2);
+                result2.StatusCode.Should().Be((System.Net.HttpStatusCode)200);
+
+                //att pet with non existent ower id
+                var jsonToAdd3 = "{\"name\":\"Damiao\",\"age\":2,\"size\":\"MEDIUM\",\"breed\":\"VIRA-LATA\",\"owerId\":99}";
+                var stringContent3 = new StringContent(jsonToAdd3, Encoding.UTF8, "application/json");
+                var result3 = await _client.PutAsync("/Pet/1", stringContent3);
+                result3.StatusCode.Should().Be((System.Net.HttpStatusCode)404);
+
+                var stringResult = result3.Content.ReadAsStringAsync().Result;
+                stringResult.Should().Contain("Ower not found");
+            }
+        }
 
 
         //[Fact]
